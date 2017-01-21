e0ed57a59cd39af5a3d101c24b069535c3c48b07
David <david.klug@isen-lille.fr>
2017-01-21 15:39:53 +0100

    SelectPost1

---
+++ GlobalSubmarine2017/Assets/scripts/UIManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public string post;
+	public bool ready = false;
+
+	public void ChoseRadar() 
+	{
+		post = "Radar"; 
+		ready = true;
+	}
+	public void ChosePilot() 
+	{
+		post = "Pilot";
+		ready = true;
+	}
+	public void ChoseEngine() 
+	{
+		post = "Engine";
+		ready = true;
+	}
+	public void ChoseBedroom() 
+	{
+		post = "Bedroom";
+		ready = true;
+	}
+	public void ChoseRadio() 
+	{
+		post = "Radio";
+		ready = true;
+	}
+	
+	public void StartRound()
+	{
+		SceneManager.LoadScene(post);
+	}
+}
